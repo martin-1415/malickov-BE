5b48b5494c052b9635d9e0427b45eb8a09711a1a
martin-1415 <martin.fischer1415@gmail.com>
2025-09-11 11:31:26 +0200

    Liquibase fixed

--- src/main/resources/db/changelog/db.changelog-1.0.sql
+++ src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -2,8 +2,6 @@
 
 
 --changeset Martin_Fischer:1 context:prod,dev
-CREATE DATABASE IF NOT EXISTS malickov;
-USE malickov;
 
 CREATE TABLE user (
                       user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -19,8 +17,8 @@ CREATE TABLE user (
 
 --changeset Martin_Fischer:2 context:dev
 INSERT INTO user (first_name, last_name, email, active, role_name, password) VALUES
-('Martin', 'Fischer', '1@1.cz',1,'DIRECTOR','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.'),
-('Sarka', 'Fischer','2@2.cz',1,'MANAGER','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.'),
-('Charlie', 'Brown','3@3.cz',1,'TEACHER','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.'),
-('Sam', 'Quited','4@4.cz',0,'PARENT','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.'),
-('Tom', 'Parent','5@5.cz',1,'PARENT','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.');
+                                                                                 ('Martin', 'Fischer', '1@1.cz',1,'DIRECTOR','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.'),
+                                                                                 ('Sarka', 'Fischer','2@2.cz',1,'MANAGER','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.'),
+                                                                                 ('Charlie', 'Brown','3@3.cz',1,'TEACHER','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.'),
+                                                                                 ('Sam', 'Quited','4@4.cz',0,'PARENT','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.'),
+                                                                                 ('Tom', 'Parent','5@5.cz',1,'PARENT','$2a$12$KeVi5xNQGqtFqALo355CMudCpNEiwDbv5O/75ML7wnqu4cdcpHzf.');
